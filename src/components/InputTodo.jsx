8cce573686ec38b822ce8f243514ac09a73f1a12
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-06 00:11:06 +0600

    data added

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 export default function InputTodo() {
   const [description, setDescription] = useState("");
-  const onSubmit = async (e) => {
-    e.prevent.default();
+  const onSubmitForm = async () => {
+    
     try {
       const body = { description };
       const response = await fetch("http://localhost:5000/todos", {
@@ -11,6 +11,7 @@ export default function InputTodo() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -19,15 +20,17 @@ export default function InputTodo() {
     <div>
       <h1 className="text-center">PERN Todo List</h1>
       <div className="flex justify-center my-5">
-        <form>
+        
           <input
             type="text"
             className="py-2 bg-gray-200 text-gray-700"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
-          <button className="px-5 py-2 bg-gray-700 ml-5">ADD</button>
-        </form>
+          <button className="px-5 py-2 bg-gray-700 ml-5" onClick={onSubmitForm}>
+            ADD
+          </button>
+        
       </div>
     </div>
   );
