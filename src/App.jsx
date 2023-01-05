1923e1bc362ef561f72b2aa69f6ff3e6905b714c
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-06 00:39:55 +0600

    list fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputTodo from './components/InputTodo'
+import ListTodo from './components/ListTodo'
 
 export default function App() {
   return (
     <div>
       <InputTodo/>
+      <ListTodo/>
     </div>
   )
 }
