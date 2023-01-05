b328a4515f8bbef6cc13d5b03fc1ef0663c3bd02
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-05 23:51:52 +0600

    form

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function InputTodo() {
   return (
     <div>
-        <h1 className='t-c'>PERN Todo List</h1>
-        <button className='p-10 bg-black'>ADD</button>
+      <h1 className="text-center">PERN Todo List</h1>
+      <div className="flex justify-center my-5">
+        <form>
+          <input type="text" className="py-2 bg-gray-200" />
+          <button className="px-5 py-2 bg-gray-700">ADD</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
