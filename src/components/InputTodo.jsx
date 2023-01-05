b28976f009766d73f3423f7af6e84c1c88257540
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-06 00:33:22 +0600

    inputTodo done

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function InputTodo() {
   const [description, setDescription] = useState("");
   const onSubmitForm = async () => {
-    
+    setDescription("");
     try {
       const body = { description };
       const response = await fetch("http://localhost:5000/todos", {
@@ -11,7 +11,6 @@ export default function InputTodo() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
-      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -20,17 +19,15 @@ export default function InputTodo() {
     <div>
       <h1 className="text-center">PERN Todo List</h1>
       <div className="flex justify-center my-5">
-        
-          <input
-            type="text"
-            className="py-2 bg-gray-200 text-gray-700"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <button className="px-5 py-2 bg-gray-700 ml-5" onClick={onSubmitForm}>
-            ADD
-          </button>
-        
+        <input
+          type="text"
+          className="py-2 bg-gray-200 text-gray-700"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="px-5 py-2 bg-gray-700 ml-5" onClick={onSubmitForm}>
+          ADD
+        </button>
       </div>
     </div>
   );
