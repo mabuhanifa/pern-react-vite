b3c640a9d5d603b20415e00f3846e9a2aa5f9c47
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-06 23:57:37 +0600

    edit todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import InputTodo from './components/InputTodo'
-import ListTodo from './components/ListTodo'
+import React from "react";
+import EditTodo from "./components/EditTodo";
+import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
 
 export default function App() {
   return (
     <div>
-      <InputTodo/>
-      <ListTodo/>
+      <InputTodo />
+      <EditTodo />
+      <ListTodo />
     </div>
-  )
+  );
 }
